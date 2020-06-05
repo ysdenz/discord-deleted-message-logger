f61a9caf2d5832b356cd2198afb66b202172aa85
ysdenz <yabondenz@outlook.com>
2020-06-05 21:14:46 +1000

    added translation feature based on google translate

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const translate = require('@vitalets/google-translate-api');
 const { stripIndents } = require('common-tags');
 const { Client, MessageEmbed } = require('discord.js');
+const lang = require('./languages.json');
+
 const client = new Client({ disableEveryone: true });
 
 require('dotenv').config()
-const { LOGS_CHANNEL_ID, BOT_TOKEN } = process.env
+const { LOGS_CHANNEL_ID, BOT_TOKEN, PREFIX } = process.env
 
 client.on('ready', () => { 
     console.log(`Bot online! ${client.user.username}`);
@@ -34,6 +37,60 @@ client.on('messageDelete', async message => {
 		info += `\n[**Attached file found. Click here**](${result[0].proxyURL})`
 	}
 	logChannel.send(embed.setDescription(info))
+})
+
+client.on('message', async message => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(PREFIX)) return;
+	const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
+
+
+	if (!args[1]) return;
+
+		const embed = new MessageEmbed()
+			.setAuthor(`${message.author.tag} (${message.author.id})`, message.author.avatarURL({ dynamic: true }))
+			.setFooter('Powered by Google Translate', 'https://i.imgur.com/dFx7Xa0.png')
+			.setTimestamp();
+
+		if (args.length > 303) return message.channel.send(embed.setColor('#7a0909').setDescription('Character count limit (including spaces) is 300.'))
+
+		await translate(args.slice(1).join(' '), { to: args[0] })
+			.then(res => {
+				Object.keys(lang).map(key => {
+					if (key === args[0]) {
+						to = lang[key].name;
+					}
+					if (key === res.from.language.iso) {
+						from = lang[key].name;
+					}
+				});
+
+				console.log(res);
+				console.log(res.text);
+				console.log(res.from.language.iso);
+
+				message.channel.send(embed
+					.setColor((message.member.displayHexColor === '#000000') ? '#fcf3f2' : message.member.displayHexColor)
+					.setDescription(stripIndents`
+					${ (res.from.text.autoCorrected || res.from.text.autoCorrected) ? `Did you mean "*${res.from.text.value}*"?` : ''}
+
+					Detected Language: **${from}**
+					${args.slice(1).join(' ')}
+
+					To: **${to}**
+					${res.text}
+				`));
+
+			})
+			.catch(err => {
+				message.channel.send(embed
+					.setTitle('Click me to view the Language Codes available')
+					.setURL('https://cloud.google.com/translate/docs/languages')
+					.setDescription(err.message)
+					.setColor('#7a0909')
+				);
+			});
+	
 })
 
 client.login(BOT_TOKEN)
