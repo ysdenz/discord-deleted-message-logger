cb9e918461f1c19412f68e1971ec10b6ce6a0d1f
ysdenz <yabondenz@outlook.com>
2020-07-06 17:42:54 +1000

    updated regex for gif filter

--- index.js
+++ index.js
@@ -43,18 +43,18 @@ client.on('message', async message => {
 	if (message.author.bot) return;
 
 	// for gif filter
-	// const regexr = (msg) => {
-	// 	if (msg.match(/https?:\/\/(.*|gyfcat.com)(\.gif)?/) && !message.member.hasPermission('MANAGE_EMOJIS')) {
-	// 		try {
-	// 			message.delete()
-	// 			message.reply('GIF usage is exclusive to server boosters <:smirky:681481290721394702>')
-	// 		} catch (error) {
-	// 			console.log(error.message);
-	// 		}
-	// 	}
-	// };
-	// if (message.content) regexr(message.content)
-	// if (message.attachments.size > 0) regexr(message.attachments.first().proxyURL)
+	const regexr = (msg) => {
+		if (msg.match(/(http(s?):)([/|.|\w|\s|-])*\.(?:gif)|(http|https):\/\/(tenor|gfycat).com\/(.*)/gm) && !message.member.hasPermission('MANAGE_EMOJIS')) {
+			try {
+				message.delete()
+				message.reply('GIF usage is exclusive to server boosters <:smirky:681481290721394702>')
+			} catch (error) {
+				console.log(error.message);
+			}
+		}
+	};
+	if (message.content) regexr(message.content)
+	if (message.attachments.size > 0) regexr(message.attachments.first().proxyURL)
 
 	if (!message.content.startsWith(PREFIX)) return;
 	const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
